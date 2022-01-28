502ed1e4de4a555887df49d3c03dcb53895b2562
siri <sirishak10962@gmail.com>
2022-01-28 18:17:38 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test_project__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
